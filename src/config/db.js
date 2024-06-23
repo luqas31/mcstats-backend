ac64498cf667f1984a31ead669025ed6acf617e3
luqas31 <lucas.ers@icloud.com>
2024-06-23 21:32:21 +0100

    chore: Update MySQL connection configuration

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 import mysql2 from 'mysql2';
 
 const db = mysql2.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "users",
-	port: "3306",
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'users',
+	port: '3306',
 });
 
 db.connect(error => {
